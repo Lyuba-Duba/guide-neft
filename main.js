ffb3ab0ed5a079e29f44afe48d4808d33f08c4ef
Andrey <andrey2455@gmail.com>
2024-08-01 06:03:13 +0700

    first iter

--- main.js
+++ main.js
@@ -1,47 +1,7 @@
-// const state = {
-//   activeCard: null
-// }
-
-// const cards = document.querySelectorAll(".card");
-
-// const handleClick = (idx) => {
-//   if (state.activeCard === idx) {
-//     cards[idx].classList.remove("active");
-//     state.activeCard = null;
-//     return
-//   }
-//   else {
-//     cards.forEach((card, i) => {
-//       card.classList.toggle("active", i === idx);
-//     })
-//   }
-//   state.activeCard = idx;
-// }
-
-// cards.forEach((card, idx) => {
-//   card.addEventListener("click", () => handleClick(idx));
-// });
-
-
-gsap.registerPlugin(ScrollTrigger);
 
 const videoContainer = document.querySelector(".video");
 const video = videoContainer.querySelector("video");
 const overlay = videoContainer.querySelector(".overlay");
-
-// Устанавливаем уровень громкости на 20%, чтобы не пугать пользователя,
-// если у него выкручены громко колонки.
-video.volume = 0.2;
-
-gsap.from(".video", {
-  scale: 0.3,
-  scrollTrigger: {
-    trigger: ".team",
-    start: "top top",
-    end: "bottom top",
-    scrub: true, 
-  }
-});
 
 
 overlay.addEventListener("click", (evt) => {
@@ -51,7 +11,6 @@ overlay.addEventListener("click", (evt) => {
 });
 
 video.addEventListener("pause", (evt) => {
-  // evt.preventDefault();
   video.pause();
   overlay.classList.remove("hidden");
 });
@@ -69,9 +28,6 @@ video.addEventListener("seeking", (evt) => {
 const money = document.querySelector(".money");
 const cover = document.querySelector(".hero-image");
 
-// const manager = document.querySelector(".manager");
-// const managerNote = document.querySelector(".manager-note");
-
 const resizeMoney = () => {
   const coverWidth = cover.clientWidth;
   const coverMargin = parseInt(window.getComputedStyle(cover).marginTop);
@@ -80,21 +36,11 @@ const resizeMoney = () => {
   money.style.marginTop = `${coverMargin + coverWidth / 6}px`;
 }
 
-// const setManagerPos = () => {
-//   const topPos = manager.offsetTop - managerNote.offsetHeight + 70;
-//   const leftPost = manager.offsetLeft + manager.offsetWidth - managerNote.offsetWidth + 500;
-
-//   managerNote.style.top = `${topPos}px`;
-//   managerNote.style.left = `${leftPost}px`;
-// }
-
 
 window.addEventListener("load", () => {
   resizeMoney();
-  // setManagerPos();
 });
 
 window.addEventListener("resize", () => {
   resizeMoney();
-  // setManagerPos();
 })
